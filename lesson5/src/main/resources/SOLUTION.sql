065a5d62dd6851dfa0b1f4406a6d62a5246aa2af
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-06-10 15:53:32 +0300

    Add files via upload

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -4,4 +4,4 @@ SELECT * FROM Student WHERE birthday > DATEADD(YEAR , -20, CURRENT_DATE) AND gro
 SELECT * FROM Student WHERE name = 'Mike' OR groupNumber = 4 OR groupNumber = 5 OR groupNumber = 6;
 SELECT * FROM Payment WHERE payment_date >= DATEADD(MONTH, -8, CURRENT_DATE);
 SELECT * FROM Student WHERE name LIKE 'A%';
-SELECT * FROM Student WHERE (name = Roxi AND groupNumber = 4) OR (name = Tallie AND groupNumber = 9);
+SELECT * FROM Student WHERE (name = 'Roxi' AND groupNumber = 4) OR (name = 'Tallie' AND groupNumber = 9);
